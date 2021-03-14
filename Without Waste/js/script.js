9173fd345c67fd8889603b72a074c7a02f8aa43a
MariVidkov <48559813+MariVidkov@users.noreply.github.com>
2021-03-14 02:50:40 +0200

    Without Index, Reciklirai and Game

--- Without Waste/js/script.js
+++ Without Waste/js/script.js
@@ -2,15 +2,11 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
     var element = document.getElementById("mainUl");
-    element.classList.add("background");  
-    var iconsNav = document.getElementById("iconsNav");    
-    iconsNav.classList.add("backgroundicons");       
+    element.classList.add("background");       
   }
   else {
     var element = document.getElementById("mainUl");    
-    element.classList.remove("background");  
-    var iconsNav = document.getElementById("iconsNav");    
-    iconsNav.classList.remove("backgroundicons");                  
+    element.classList.remove("background");                 
   }
 }
 
